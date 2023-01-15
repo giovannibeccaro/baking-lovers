511bcb060b3ecd8ada9c41b978c914fed55b351b
Giovanni Beccaro <gio.beccaro@gmail.com>
2023-01-15 22:53:26 +0100

    added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar/Navbar";
 import "../styles/index.scss";
 import { SessionProvider } from "next-auth/react";
+import NextNProgress from "nextjs-progressbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <SessionProvider session={pageProps.session}>
         <Navbar />
+        <NextNProgress color="#ca5369" height={5} />
         <Component {...pageProps} />
       </SessionProvider>
     </>
